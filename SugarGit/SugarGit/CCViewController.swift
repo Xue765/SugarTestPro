469a67955e7cc75d4dab55ec78b0515d3b467a93
sugarcode <765950216@qq.com>
2020-08-30 16:41:53 +0800

    添加CCVC

---
+++ SugarGit/SugarGit/CCViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CCViewController.swift
+//  SugarGit
+//
+//  Created by martmoney on 2020/8/30.
+//  Copyright © 2020 sugdev. All rights reserved.
+//
+
+import UIKit
+
+class CCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
