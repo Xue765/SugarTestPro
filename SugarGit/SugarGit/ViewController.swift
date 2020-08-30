d946626c2e85dcb441ac5e0360f885d490d13a8c
sugarcode <765950216@qq.com>
2020-08-30 16:36:23 +0800

    1.初始化项目

---
+++ SugarGit/SugarGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SugarGit
+//
+//  Created by martmoney on 2020/8/30.
+//  Copyright © 2020 sugdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
