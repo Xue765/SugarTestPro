9bffa07efe685612bb898baf9e24051d3c58e673
sugarcode <765950216@qq.com>
2020-08-30 16:40:42 +0800

    BBVC添加约束

---
+++ SugarGit/SugarGit/BBViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BBViewController.swift
+//  SugarGit
+//
+//  Created by martmoney on 2020/8/30.
+//  Copyright © 2020 sugdev. All rights reserved.
+//
+
+import UIKit
+
+class BBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
